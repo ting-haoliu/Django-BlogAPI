c348ef13295ed4d29e2a0491652f27dfff0b64e3
Ting Hao Liu <s0129qkak@outlook.com>
2024-09-08 13:42:29 -0700

    Create database and display data on the web pages

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
-posts = [
-    {
-        'author': 'Tinghao',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Tinghao Liu',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
